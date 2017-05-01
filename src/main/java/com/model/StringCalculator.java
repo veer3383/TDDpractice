6473634c81f60e0413cc6d18d98a1bbaec92af98
Viraj Ajgaonkarb <viraj.ajgaonkar@presentsoft.co.in>
2017-05-01 16:33:57 +0530

    first commit

---
+++ src/main/java/com/model/StringCalculator.java
@@ -0,0 +1,39 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Viraj on 01-05-2017.
+ */
+public class StringCalculator {
+    public static int add(final String numbers) {
+        String delimiter = ",|n";
+        String numbersWithoutDelimiter = numbers;
+        if (numbers.startsWith("//")) {
+            int delimiterIndex = numbers.indexOf("//") + 2;
+            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
+            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("n") + 1);
+        }
+        return add(numbersWithoutDelimiter, delimiter);
+    }
+    private static int add(final String numbers,final String delimiter) { // Changed void to int
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(delimiter);
+        List negativeNumbers = new ArrayList();
+        for (String number : numbersArray) {
+            if (!number.trim().isEmpty()) {
+                int numberInt = Integer.parseInt(number.trim());
+                if (numberInt < 0) {
+                    negativeNumbers.add(numberInt);
+                } else if (numberInt <= 1000) {
+                    returnValue += numberInt;
+                }
+            }
+        }
+        if (negativeNumbers.size() > 0) {
+            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+        }
+        return returnValue;
+    }
+}
